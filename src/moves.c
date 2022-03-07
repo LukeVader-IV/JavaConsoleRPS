865ded9d1f01c0c9fbbf671c8f79a90f901ee8b0
Luke Vader <cont-misc.xm2mj@aleeas.com>
2022-03-07 09:54:55 +0100

    more efficient win calculator after some testing the fastest computation was using a 2d array here are the results of all my tests switch:    19:50.56 if-el-if:  19:56.23 2D Array:  19:48.68 1D Array:  19:38.46 modulo:    19:50.44 these tests were run in a loop 1000000000 times over on a mid-high end desktop, any difference between these are not realistically noticable

--- src/moves.c
+++ src/moves.c
@@ -3,13 +3,13 @@
 /* returns: 0 - loss   1 - draw    2 - victory   3 - exiting   5 - ERROR*/
 /* input:   0 - rock   1 - paper   2 - scisors */
 
-/*				Win-Loss						substraction table	*/
+/*        Win-Loss            Resulting Table      */
 
-/*		 		|	R	|	P	|	S						|	0	|	1	|	2			 */
-/*		 -------------- 				--------------		 */
-/*		 R	|	D	|	L	|	W 				0	|	0	|-1	|-2 			*/
-/*		 P	|	W	|	D	| L 				1	|	1	|	0	|-1 			*/
-/*		 S	|	L	|	W	|	L 				2	|	2	|	1	|	0 			*/
+/*      | R | P | S             | 0 | 1 | 2        */
+/*    --------------          --------------       */
+/*    R | D | L | W           0 | 1 | 0 | 2       */
+/*    P | W | D | L           1 | 2 | 1 | 0        */
+/*    S | L | W | L           2 | 0 | 2 | 1        */
 
 int checkwin (int player, int opponent){
 
@@ -18,19 +18,6 @@ int checkwin (int player, int opponent){
 	else if (player > 2 || opponent > 2)
 		return 5;
 
-	switch (player - opponent)
-		{
-		case 0:
-			return 1;
-		case 1:
-		case -2:
-			return 2;
-
-		case 2:
-		case -1:
-			return 0;
-
-		default:
-			return 5;
-		}
+	int results[5] = {0, 2, 1, 0, 2};
+	return results[player + 2 - opponent];
 }
